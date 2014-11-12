156bc4741db98f9f0aff7c8a81b5bb3b917f12e5
Aditya Prakash <aditya.prakash132@gmail.com>
2014-11-12 19:34:56 +0530

    add conf

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :new_threads
   has_many :replies
