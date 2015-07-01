8b6d6f42592ad6987cf328c8c9b3a5061f89de6f
Aditya Prakash <aditya.prakash132@gmail.com>
2015-07-01 23:53:29 +0530

    Add factory_girl and fix tests of new_thread controller spec
    Default_url_option was added to fix
    'Missing host to link to! Please provide the :host parameter' error.
    Factories was added for user and new_project.

--- Gemfile
+++ Gemfile
@@ -64,6 +64,7 @@ gem 'friendly_id', '~> 5.1.0'
 group :development, :test do
   gem 'rspec-rails', '~> 3.0'
   gem 'letter_opener'
+  gem 'factory_girl_rails'
 end
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- spec/controllers/new_threads_controller_spec.rb
+++ spec/controllers/new_threads_controller_spec.rb
@@ -1,12 +1,9 @@
 require 'spec_helper'
 
-RSpec.describe NewThreadsController, type: :controller do
+describe NewThreadsController do
+  let(:new_thread) { create(:new_thread) }
+
   context 'user is guest' do
-
-    before do
-      @new_thread = NewThread.create(title: 'test thread', description: 'this is a test thread', user_id: 2)
-    end
-
     it 'can see the index page' do
       get :index
       expect(response).to render_template('list')
@@ -14,28 +11,22 @@ RSpec.describe NewThreadsController, type: :controller do
 
     describe 'GET #show' do
       it 'renders show template' do
-        get :show, id: @new_thread.slug
+        get :show, id: new_thread.slug
         expect(response).to render_template('show')
       end
     end
 
     it 'can not see new thread page' do
       get :new
-      expect(response).not_to render_template('show')
-      expect(response).to redirect_to new_user_session_path
+      expect(response).not_to render_template('new')
     end
   end
 
   context 'user is signed in' do
 
     describe 'allows new, create and show actions' do
-
-      before do
-        @request.env["devise.mapping"] = Devise.mappings[:user]
-        @user = User.create(username: 'test_user', email: 'test@test.com', password: '12345',  password_confirmation: '12345')
-        puts @user.id
-        sign_in(@user)
-      end
+      let(:user) { create(:user) }
+      before { sign_in(user) }
 
       it 'can see new thread page' do
         get :new
@@ -44,9 +35,12 @@ RSpec.describe NewThreadsController, type: :controller do
 
       describe 'can create new threads' do
         it 'creates new thread' do
-          post :create, new_thread: { title: 'test thread', description: 'this is a test thread', user_id: @user.id }
-          #@user.reload
-          expect(response).to redirect_to(@user.new_threads.first.urlbase)
+          post :create, new_thread: {
+            title: new_thread.title,
+            description: new_thread.description,
+            user_id: user.id
+          }
+          expect(user.new_threads).not_to be_empty
         end
       end
     end
